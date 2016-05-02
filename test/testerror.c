c86fd7d0a076c88d5f7e554b4cb8b76a59df435f
huiyu.eun <huiyu.eun@samsung.com>
2016-05-02 09:48:50 +0900

    change SDL 1.2 to SDL 2.0
    [Title] change SDL 1.2 to SDL 2.0
     - move SDL 1.2 Source to new branch 'SDL_1.2'
    
    [Team] Graphics
    
    Change-Id: Ic28c8ddc04dc821fa42f815e9a7e67037dbeb07c

--- test/testerror.c
+++ test/testerror.c
@@ -1,3 +1,14 @@
+/*
+  Copyright (C) 1997-2016 Sam Lantinga <slouken@libsdl.org>
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely.
+*/
 
 /* Simple test of the SDL threading code and error handling */
 
@@ -6,56 +17,61 @@
 #include <signal.h>
 
 #include "SDL.h"
-#include "SDL_thread.h"
 
 static int alive = 0;
 
 /* Call this instead of exit(), so we can clean up SDL: atexit() is evil. */
-static void quit(int rc)
+static void
+quit(int rc)
 {
-	SDL_Quit();
-	exit(rc);
+    SDL_Quit();
+    exit(rc);
 }
 
-int SDLCALL ThreadFunc(void *data)
+int SDLCALL
+ThreadFunc(void *data)
 {
-	/* Set the child thread error string */
-	SDL_SetError("Thread %s (%d) had a problem: %s",
-			(char *)data, SDL_ThreadID(), "nevermind");
-	while ( alive ) {
-		printf("Thread '%s' is alive!\n", (char *)data);
-		SDL_Delay(1*1000);
-	}
-	printf("Child thread error string: %s\n", SDL_GetError());
-	return(0);
+    /* Set the child thread error string */
+    SDL_SetError("Thread %s (%lu) had a problem: %s",
+                 (char *) data, SDL_ThreadID(), "nevermind");
+    while (alive) {
+        SDL_Log("Thread '%s' is alive!\n", (char *) data);
+        SDL_Delay(1 * 1000);
+    }
+    SDL_Log("Child thread error string: %s\n", SDL_GetError());
+    return (0);
 }
 
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
-	SDL_Thread *thread;
+    SDL_Thread *thread;
 
-	/* Load the SDL library */
-	if ( SDL_Init(0) < 0 ) {
-		fprintf(stderr, "Couldn't initialize SDL: %s\n",SDL_GetError());
-		return(1);
-	}
+    /* Enable standard application logging */
+    SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO);
 
-	/* Set the error value for the main thread */
-	SDL_SetError("No worries");
+    /* Load the SDL library */
+    if (SDL_Init(0) < 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s\n", SDL_GetError());
+        return (1);
+    }
 
-	alive = 1;
-	thread = SDL_CreateThread(ThreadFunc, "#1");
-	if ( thread == NULL ) {
-		fprintf(stderr, "Couldn't create thread: %s\n", SDL_GetError());
-		quit(1);
-	}
-	SDL_Delay(5*1000);
-	printf("Waiting for thread #1\n");
-	alive = 0;
-	SDL_WaitThread(thread, NULL);
+    /* Set the error value for the main thread */
+    SDL_SetError("No worries");
 
-	printf("Main thread error string: %s\n", SDL_GetError());
+    alive = 1;
+    thread = SDL_CreateThread(ThreadFunc, NULL, "#1");
+    if (thread == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create thread: %s\n", SDL_GetError());
+        quit(1);
+    }
+    SDL_Delay(5 * 1000);
+    SDL_Log("Waiting for thread #1\n");
+    alive = 0;
+    SDL_WaitThread(thread, NULL);
 
-	SDL_Quit();
-	return(0);
+    SDL_Log("Main thread error string: %s\n", SDL_GetError());
+
+    SDL_Quit();
+    return (0);
 }
