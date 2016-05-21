505a6188ea69c1e2be120c68736eb23381e9c6a8
Sangjin Lee <lsj119@samsung.com>
2016-05-21 12:51:39 +0900

    Redesign tizen backend
    current sdl backend for tizen use wayland.
    but tizen backend change to EFL.
    
    Change-Id: I8e5ba33662bc57145035fc45580e98ae0a82b19e

---
+++ src/video/tizen/SDL_tizenvideo.c
@@ -0,0 +1,235 @@
+/*
+  Simple DirectMedia Layer
+  Copyright (C) 1997-2016 Sam Lantinga <slouken@libsdl.org>
+  Copyright 2015 Samsung Electronics co., Ltd. All Rights Reserved.
+
+  Contact: Sangjin Lee <lsj119@samsung.com>
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+
+#include "../../SDL_internal.h"
+
+#if SDL_VIDEO_DRIVER_TIZEN
+
+#include "SDL_video.h"
+#include "SDL_mouse.h"
+#include "SDL_stdinc.h"
+#include "../../events/SDL_events_c.h"
+
+#include "SDL_tizenvideo.h"
+#include "SDL_tizenevents_c.h"
+#include "SDL_tizenwindow.h"
+#include "SDL_tizenopengles.h"
+#include "SDL_tizenmouse.h"
+#include "SDL_tizentouch.h"
+
+#define TIZENVID_DRIVER_NAME "tizen"
+
+/* Initialization/Query functions */
+static int
+Tizen_VideoInit(_THIS);
+
+static void
+Tizen_GetDisplayModes(_THIS, SDL_VideoDisplay *sdl_display);
+static int
+Tizen_SetDisplayMode(_THIS, SDL_VideoDisplay *display, SDL_DisplayMode *mode);
+
+static void
+Tizen_VideoQuit(_THIS);
+
+static int
+__tizen_app_create(void *data)
+{
+    fprintf(stderr, "%s:%d\n",__FUNCTION__, __LINE__);
+    return 0;
+}
+
+static int
+__tizen_app_terminate(void *data)
+{
+    fprintf(stderr, "%s:%d\n",__FUNCTION__, __LINE__);
+    return 0;
+}
+
+static int
+__tizen_app_pause (void *data)
+{
+    fprintf(stderr, "%s:%d\n",__FUNCTION__, __LINE__);
+    return 0;
+}
+
+static int
+__tizen_app_resume(void *data)
+{
+    fprintf(stderr, "%s:%d\n",__FUNCTION__, __LINE__);
+    return 0;
+}
+
+static int
+__tizen_app_reset(bundle *bun, void *data)
+{
+    fprintf(stderr, "%s:%d\n",__FUNCTION__, __LINE__);
+    return 0;
+}
+
+static void
+__tizen_add_display(SDL_VideoData *d, uint32_t id)
+{
+    SDL_VideoDisplay display;
+    SDL_DisplayMode mode;
+    static char *display_name = "tizen";
+
+    SDL_zero(display);
+    SDL_zero(mode);
+
+    display.name = display_name;
+
+    ecore_wl_screen_size_get(&mode.w, &mode.h);
+    mode.refresh_rate = 60; //Hz
+    SDL_AddDisplayMode(&display, &mode);
+
+    display.current_mode = mode;
+    display.desktop_mode = mode;
+
+    SDL_AddVideoDisplay(&display);
+}
+
+/* Wayland driver bootstrap functions */
+static int
+Tizen_Available(void)
+{
+    return 1;
+}
+
+static void
+Tizen_DeleteDevice(SDL_VideoDevice *device)
+{
+    SDL_free(device);
+}
+
+static SDL_VideoDevice *
+Tizen_CreateDevice(int devindex)
+{
+    SDL_VideoDevice *device;
+
+    /* Initialize all variables that we clean on shutdown */
+    device = SDL_calloc(1, sizeof(SDL_VideoDevice));
+    if (!device) {
+        SDL_OutOfMemory();
+        return NULL;
+    }
+
+    /* Tizen video */
+    device->VideoInit				= Tizen_VideoInit;
+    device->VideoQuit 				= Tizen_VideoQuit;
+    device->SetDisplayMode 			= Tizen_SetDisplayMode;
+    device->GetDisplayModes 		= Tizen_GetDisplayModes;
+    device->free					= Tizen_DeleteDevice;
+
+    device->PumpEvents = Tizen_PumpEvents;
+
+    device->GL_SwapWindow = Tizen_GLES_SwapWindow;
+    device->GL_GetSwapInterval = Tizen_GLES_GetSwapInterval;
+    device->GL_SetSwapInterval = Tizen_GLES_SetSwapInterval;
+    device->GL_MakeCurrent = Tizen_GLES_MakeCurrent;
+    device->GL_CreateContext = Tizen_GLES_CreateContext;
+    device->GL_LoadLibrary = Tizen_GLES_LoadLibrary;
+    device->GL_UnloadLibrary = Tizen_GLES_UnloadLibrary;
+    device->GL_GetProcAddress = Tizen_GLES_GetProcAddress;
+    device->GL_DeleteContext = Tizen_GLES_DeleteContext;
+
+    device->CreateWindow = Tizen_CreateWindow;
+    device->ShowWindow = Tizen_ShowWindow;
+    device->SetWindowFullscreen = Tizen_SetWindowFullscreen;
+    device->SetWindowSize = Tizen_SetWindowSize;
+    device->DestroyWindow = Tizen_DestroyWindow;
+    device->SetWindowHitTest = Tizen_SetWindowHitTest;
+    device->GetWindowWMInfo			= Tizen_GetWindowWMInfo;
+
+    return device;
+}
+
+VideoBootStrap TIZEN_bootstrap = {
+    TIZENVID_DRIVER_NAME, "SDL tizen video driver",
+    Tizen_Available, Tizen_CreateDevice
+};
+
+int
+Tizen_VideoInit(_THIS)
+{
+    SDL_VideoData *data = SDL_malloc(sizeof * data);
+	int argc = 1;
+	char *_argv[] = {
+		"tizen_sdl_app",
+		NULL,
+	};
+	char **argv;
+	struct appcore_ops ops = {
+        .data           = _this,
+        .create         = __tizen_app_create,
+        .terminate      = __tizen_app_terminate,
+        .pause          = __tizen_app_pause,
+        .resume         = __tizen_app_resume,
+		.reset          = __tizen_app_reset,
+	};
+
+	argv = _argv;
+
+    if (data == NULL)
+        return SDL_OutOfMemory();
+    memset(data, 0, sizeof * data);
+
+    _this->driverdata = data;
+
+    ecore_wl_init(NULL);
+    __tizen_add_display(data, 0);
+
+    Tizen_InitWindow(_this);
+
+    appcore_efl_init(_argv[0], &argc, &argv, &ops);
+    return 0;
+}
+
+static void
+Tizen_GetDisplayModes(_THIS, SDL_VideoDisplay *sdl_display)
+{
+}
+
+static int
+Tizen_SetDisplayMode(_THIS, SDL_VideoDisplay *display, SDL_DisplayMode *mode)
+{
+    return SDL_Unsupported();
+}
+
+void
+Tizen_VideoQuit(_THIS)
+{
+    SDL_VideoData *data = _this->driverdata;
+
+    Tizen_DeinitWindow(_this);
+    appcore_efl_fini();
+    ecore_wl_shutdown();
+    free(data);
+
+    _this->driverdata = NULL;
+}
+
+
+#endif /* SDL_VIDEO_DRIVER_TIZEN */
+
+/* vi: set ts=4 sw=4 expandtab: */
